7e45b3205a720a7347098d9f2b97de1813783a40
chocs2go <56859132+chocs2go@users.noreply.github.com>
2019-11-06 20:49:00 -0800

    Add files via upload

---
+++ labs/lab3/d_ff.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+/**************************************************************
+*
+* Author: 		A. Paguio
+* Email: 		anthony.paguio@student.csulb.edu
+* Filename: 	top_level.v
+* Date: 			February 13, 2019
+* Version: 		1
+* 
+* Description: The function of the D flip flop is to accept 
+*					the input data on the positive edge of the 
+*					clock and hold it until it is needed.
+*
+***************************************************************/
+
+module d_ff(clk, reset, D, Q);
+	input clk, reset, D;
+   
+	output Q;
+	reg  Q; 
+  
+   always@(posedge clk or posedge reset) begin
+		if (reset) begin
+			Q <= 1'b0;
+		end //end if 
+		
+	   else 
+			Q <= D;
+	end //end always
+
+endmodule
---
+++ labs/lab3/debounce.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// File Name: debounce.v
+// Project:  CECS 301 Lab 3
+// Designer: Anthony Paguio 
+// Email: anthony.paguio@student.csulb.edu
+// Rev. Date: March 7, 2019
+//
+// Purpose: The purpose of the debounce module is to debounce the input signal 
+//				after pressing the btn_down from the Nexys A7.
+//				When btn_down is pressed, the signal does not immediately rise from 
+//				low to high but it bounces from low to high. It waits for the bounce
+//				to stop until it recognizes the button was pushed. 
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module debounce(D_in, clk_in, reset, D_out);
+	input D_in, clk_in, reset;
+	
+	output D_out;
+	wire D_out;
+	
+	reg q9, q8, q7, q6, q5, q4, q3, q2, q1, q0;
+	
+	always @ (posedge clk_in or posedge reset) begin
+		if (reset == 1'b1) begin
+			{q9, q8, q7, q6, q5, q4, q3, q2, q1, q0} <= 10'b0;
+		end //end if
+		
+		else begin
+			//shift in the new sample that's on the D_in input
+			q9 <= q8; q8 <= q7; q7 <= q6; q6 <= q5; q5 <= q4;
+			q4 <= q3; q3 <= q2; q2 <= q1; q1 <= q0; q0 <= D_in;
+		end //end else
+	end //end always 
+	
+	//create the debounced, one-shot pulse
+	assign D_out = !q9 & q8 & q7 & q6 & q5 &
+						q4 & q3 & q2 & q1 & q0; 
+
+
+endmodule
