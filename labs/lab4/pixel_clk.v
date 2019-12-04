b77e3ee57a750ede2f873e40f81334c981dab94d
chocs2go <56859132+chocs2go@users.noreply.github.com>
2019-12-03 22:31:11 -0800

    Add files via upload

---
+++ labs/lab4/pixel_clk.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// File Name: clk_div.v
+// Project:  CECS 301 Lab 3
+// Designer: Anthony Paguio 
+// Email: anthony.paguio@student.csulb.edu
+// Rev. Date: March 7, 2019
+//
+// Purpose:  The purpose of the clock divider is to slow down the internal clock 
+//				 based on the incoming frequency of 100MHz and the outgoing 
+//				 frequency of 480MHz.
+////////////////////////////////////////////////////////////////////////////////
+
+module pixel_clk(clk_in, reset, clk_out);
+
+	input   clk_in, reset;
+	
+	output  clk_out;
+	reg     clk_out;
+	
+	integer i; 
+	
+	//**************************************************************
+	// The following verilog code will "divide" an incoming clock
+	// by the 32-bit decimal value specified in the "if condition"
+	//
+	// The value of the counter that counts the incoming clock ticks 
+	// is equal to [ (Incoming Freq / Outgoing Freq) / 2 ]
+	//**************************************************************
+
+	always @ (posedge clk_in or posedge reset) begin
+	
+		if (reset == 1'b1) begin
+			i = 0;
+			clk_out = 0;
+		end
+		//got a clock, so increment the counter and
+		//test to see if half a period has elapsed
+		
+		else begin
+			i = i + 1;
+			if (i >= (100 * (10**6)) / (2 * 480)) begin
+				clk_out = ~clk_out;
+				i = 0;
+			end // end if
+		end // end else
+		
+	end // end always
+
+endmodule
---
+++ labs/lab4/pixel_controller.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    05:58:39 03/13/2019 
+// Design Name: 
+// Module Name:    pixel_controller 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module pixel_controller(clk, reset, anodes, sel[2:0], D, Q);
+    
+    input clk, reset;
+    
+    output reg [7:0] anodes;
+    output reg [2:0] sel;
+    
+    reg [2:0] D, Q;
+    
+    //**************************//
+    //  Next-State Combo Logic  //
+    //**************************//
+    always @ ( Q )
+        case ( Q )
+            3'b000 : D = 3'b001;
+            3'b001 : D = 3'b010;
+            3'b010 : D = 3'b011;
+            3'b011 : D = 3'b100;
+            3'b100 : D = 3'b101;
+            3'b101 : D = 3'b110;
+            3'b110 : D = 3'b111;
+            3'b111 : D = 3'b000;
+            default: D = 3'b000;
+        endcase
+    
+    //************************//
+    //  State Register Logic  //
+    //************************//
+    always @ (posedge clk or posedge reset)
+        if (reset == 1'b1)
+            Q = 3'b0;
+        else
+            Q = D;
+            
+    //*********************//
+    //  Ouput Combo Logic  //
+    //*********************//
+    always @ ( Q )
+        case ( Q )
+            3'b000 : { an, seg_sel } = 11'b01111111111;
+            3'b001 : { an, seg_sel } = 11'b10111111110;
+            3'b010 : { an, seg_sel } = 11'b11011111101;
+            3'b011 : { an, seg_sel } = 11'b11101111100;
+            3'b100 : { an, seg_sel } = 11'b11110111011;
+            3'b101 : { an, seg_sel } = 11'b11111011010;
+            3'b110 : { an, seg_sel } = 11'b11111101001;
+            3'b111 : { an, seg_sel } = 11'b11111110000;
+            default: { an, seg_sel } = 11'b01111111111;
+        endcase
+		  
+endmodule
