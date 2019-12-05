ff9983b70c23755d49da7a548bd8195c52bb8161
chocs2go <56859132+chocs2go@users.noreply.github.com>
2019-12-04 19:36:35 -0800

    Add files via upload

---
+++ labs/lab4/ad_mux.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:18:14 03/11/2019 
+// Design Name: 
+// Module Name:    ad_mux 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ad_mux(d7, d6, d5, d4, d3, d2, d1, d0, sel, Y );
+
+    input      [3:0] d7, d6, d5, d4, d3, d2, d1, d0;
+    input      [2:0] sel;
+    
+    output reg [3:0] Y;
+    
+    always @ ( sel )
+        case ( sel )
+            3'b000 : Y = d0;
+            3'b001 : Y = d1;
+            3'b010 : Y = d2;
+            3'b011 : Y = d3;
+            3'b100 : Y = d4;
+            3'b101 : Y = d5;
+            3'b110 : Y = d6;
+            3'b111 : Y = d7;
+            default: Y = 4'h0;
+        endcase
+
+
+endmodule
---
+++ labs/lab4/hex_to_7segment.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// File Name: hex_to_7segment.v
+// Project:  CECS 301 Lab 3
+// Designer: Anthony Paguio 
+// Email: anthony.paguio@student.csulb.edu
+// Rev. Date: March 7, 2019
+//
+// Purpose: The purpose of the the hex to 7 segment module is to display the 
+//				segments	0-7 and a-g on the Nexys board. 
+//
+////////////////////////////////////////////////////////////////////////////////
+
+module hex_to_7segment(hex, a, b, c, d, e, f, g);
+
+	input [3:0] hex;
+	
+	output reg a, b, c, d, e, f, g;
+
+	always@ (hex) begin
+		case (hex)
+		
+
+			4'b0000 : {a,b,c,d,e,f,g} = 7'b0000001; // Display 0
+			4'b0001 : {a,b,c,d,e,f,g} = 7'b1001111; // Display 1
+			4'b0010 : {a,b,c,d,e,f,g} = 7'b0010010; // Display 2
+			4'b0011 : {a,b,c,d,e,f,g} = 7'b0000110; // Display 3
+			4'b0100 : {a,b,c,d,e,f,g} = 7'b1001100; // Display 4
+			4'b0101 : {a,b,c,d,e,f,g} = 7'b0100100; // Display 5
+			4'b0110 : {a,b,c,d,e,f,g} = 7'b0100000; // Display 6
+			4'b0111 : {a,b,c,d,e,f,g} = 7'b0001111; // Display 7
+			4'b1000 : {a,b,c,d,e,f,g} = 7'b0000000; // Display 8
+			4'b1001 : {a,b,c,d,e,f,g} = 7'b0000100; // Display 9
+			4'b1010 : {a,b,c,d,e,f,g} = 7'b0001000; // Display A
+			4'b1011 : {a,b,c,d,e,f,g} = 7'b1100000; // Display B
+			4'b1100 : {a,b,c,d,e,f,g} = 7'b0110001; // Display C
+			4'b1101 : {a,b,c,d,e,f,g} = 7'b1000010; // Display D
+			4'b1110 : {a,b,c,d,e,f,g} = 7'b0110000; // Display E
+			4'b1111 : {a,b,c,d,e,f,g} = 7'b0111000; // Display F
+			default : {a,b,c,d,e,f,g} = 7'b1111010; // Display 'r' for error.
+		
+		endcase
+		
+	
+	end
+	
+endmodule
